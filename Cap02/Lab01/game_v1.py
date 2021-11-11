fd998b53503951e3fdd3f0302234fc9cdbddb33d
evandro.avellaavell <>
2021-11-11 20:07:44 -0300

    Atualização para atividades do Level Up

--- Cap02/Lab01/game_v1.py
+++ Cap02/Lab01/game_v1.py
@@ -1,27 +1,36 @@
 # Game Ping-Pong
+# Objetivo da Atividade: Incluir comentários explicando o código
 
-from tkinter import *
+# Importando as os módulos que serão utilizados
+from tkinter import Tk, Canvas
 import random
 import time
 
+# Pergunta ao jogador o nível de jogo e guarda o valor na variável level
 level = int(input("Qual nível você gostaria de jogar? 1/2/3/4/5 \n"))
+
+# Cria uma variável length a partir da variável level
 length = 500/level
 
-
+# Instancia o objeto root a partir da classe Tk()
 root = Tk()
 root.title("Ping Pong")
-root.resizable(0,0)
+root.resizable(0, 0)
 root.wm_attributes("-topmost", -1)
 
-canvas = Canvas(root, width=800, height=600, bd=0,highlightthickness=0)
+# Instancia o objeto canvas a partir da classe Canvas
+canvas = Canvas(root, width=800, height=600, bd=0, highlightthickness=0)
 canvas.pack()
 
+# Chama o método update do objeto root
 root.update()
 
-# Variável
+# Cria as variáveis count do tipo int e lost do tipo bool
 count = 0
 lost = False
 
+
+# Cria a classe Bola com seus atributos e métodos
 class Bola:
     def __init__(self, canvas, Barra, color):
         self.canvas = canvas
@@ -38,10 +47,8 @@ class Bola:
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
 
-
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
-
         pos = self.canvas.coords(self.id)
 
         if pos[1] <= 0:
@@ -52,20 +59,18 @@ class Bola:
 
         if pos[0] <= 0:
             self.x = 3
-            
+
         if pos[2] >= self.canvas_width:
             self.x = -3
 
         self.Barra_pos = self.canvas.coords(self.Barra.id)
 
-
         if pos[2] >= self.Barra_pos[0] and pos[0] <= self.Barra_pos[2]:
             if pos[3] >= self.Barra_pos[1] and pos[3] <= self.Barra_pos[3]:
                 self.y = -3
                 global count
-                count +=1
+                count += 1
                 score()
-
 
         if pos[3] <= self.canvas_height:
             self.canvas.after(10, self.draw)
@@ -75,6 +80,7 @@ class Bola:
             lost = True
 
 
+# Cria a classe Barra com seus atributos e métodos
 class Barra:
     def __init__(self, canvas, color):
         self.canvas = canvas
@@ -95,13 +101,13 @@ class Barra:
 
         if self.pos[0] <= 0:
             self.x = 0
-        
+
         if self.pos[2] >= self.canvas_width:
             self.x = 0
-        
+
         global lost
-        
-        if lost == False:
+
+        if not False:
             self.canvas.after(10, self.draw)
 
     def move_left(self, event):
@@ -113,36 +119,38 @@ class Barra:
             self.x = 3
 
 
+# Cria a função start_game()
 def start_game(event):
     global lost, count
     lost = False
     count = 0
     score()
     canvas.itemconfig(game, text=" ")
-
     time.sleep(1)
     Barra.draw()
     Bola.draw()
 
 
+# Cria a função score()
 def score():
     canvas.itemconfig(score_now, text="Pontos: " + str(count))
 
+
+# Cria a função game_over()
 def game_over():
     canvas.itemconfig(game, text="Game over!")
 
 
+# Instancia os objetos Barra e Bola
 Barra = Barra(canvas, "orange")
 Bola = Bola(canvas, Barra, "purple")
 
-
-score_now = canvas.create_text(430, 20, text="Pontos: " + str(count), fill = "green", font=("Arial", 16))
+# Invoca métodos dos objetos canvas e root
+score_now = canvas.create_text(430, 20, text="Pontos: " + str(count),
+                               fill="green", font=("Arial", 16))
 game = canvas.create_text(400, 300, text=" ", fill="red", font=("Arial", 40))
 
 
 canvas.bind_all("<Button-1>", start_game)
 
 root.mainloop()
-
-
-
